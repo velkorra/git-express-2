c2394f43b825e9d20a54b2bae855ded761af6313
velkorra <anton.s.vertunov@gmail.com>
2024-12-19 19:06:58 +0300

    add dotenv lib and move port to env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+const PORT = process.env.PORT
 
 app.use(express.json());
 
